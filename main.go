5f040657f07f0d95f8ec00a3abe27092039d712b
OnkelPony <zkurvenej@outlook.com>
2022-03-20 12:27:34 +0100

    Custom routing.

--- main.go
+++ main.go
@@ -15,14 +15,26 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p><a href=\"mailto:zwo@centrum.cz\">Napiš mi</a>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Page not found, jako fakt ne...</h1>")
+}
 func pathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, r.URL.Path)
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "contacts":
+		contactHandler(w, r)
+	default:
+		notFoundHandler(w, r)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", pathHandler)
 	//	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contacts", contactHandler)
+	// http.HandleFunc("/contacts", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
