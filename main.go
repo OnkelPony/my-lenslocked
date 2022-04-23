9211023009914e887d149ddef054407c63c268a4
OnkelPony <zkurvenej@outlook.com>
2022-04-23 16:24:38 +0200

    Implemented StaticHandler in  controllers package.

--- main.go
+++ main.go
@@ -2,38 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/OnkelPony/my-lenslocked/controllers"
 	"github.com/OnkelPony/my-lenslocked/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"log"
 	"net/http"
 	"path/filepath"
 )
-
-func executeTemplate(w http.ResponseWriter, filePath string) {
-	tpl, err := views.Parse(filePath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing template.", http.StatusInternalServerError)
-		return
-	}
-	tpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath)
-}
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
@@ -47,9 +22,25 @@ func galleryHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Heartbeat("/ping"))
-	r.Get("/", homeHandler)
-	r.With(middleware.Logger).Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.With(middleware.Logger).Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.Get("/gallery/{userID}", galleryHandler)
 	r.With(middleware.Logger).NotFound(notFoundHandler)
 	fmt.Println("Starting the server on :3000...")
