56ace65b443c32ad5527406031d20bc55d2cdf52
Jiří Martínek <zwo@centrum.cz>
2022-04-29 21:29:31 +0200

    Implemented named templates.

--- controllers/static.go
+++ controllers/static.go
@@ -2,11 +2,44 @@ package controllers
 
 import (
 	"github.com/OnkelPony/my-lenslocked/views"
+	"html/template"
 	"net/http"
 )
 
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	qAndA := []struct {
+		Question string
+		Answer   template.HTML
+		QType    string
+	}{
+		{
+			Question: "Jak se máš?",
+			Answer:   "De to!",
+			QType:    "blbá",
+		},
+		{
+			Question: "Za kolik jdeš?",
+			Answer:   "Za pade.",
+			QType:    "přímá",
+		},
+		{
+			Question: "Is anal included?",
+			Answer:   "Of course!",
+			QType:    "chytrá",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+			QType:    "praktická",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, qAndA)
 	}
 }
